4a17750a8b42079b73379fae884291916923fe0b
godcong <jumbycc@163.com>
2022-02-11 18:01:26 +0800

    feat(module): add module interface

--- module/module.go
+++ module/module.go
@@ -1,14 +1,10 @@
 package module
 
-import (
-	"github.com/tikalink/of"
-)
-
 //Name returns the names of all the modules
 //ENUM(bootstrap,instruct,max)
 type Name int
 
-type TypeHandleFunc = func(id of.ID, data []byte) error
+type TypeHandleFunc = func(id string, data []byte) error
 
 type Module interface {
 	Start() error
