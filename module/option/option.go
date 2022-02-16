78e81ec67ae627075676d2ac997c23172f8959fc
godcong <jumbycc@163.com>
2022-02-16 14:04:46 +0800

    feat(module): add module interface

--- module/option/option.go
+++ module/option/option.go
@@ -1,7 +1,12 @@
 package option
 
+import (
+	"github.com/tikalink/of"
+)
+
 type Option interface {
 	Repo() string
+	FatherHandler() func(father of.ID)
 }
 
 func Repo(repo string) func(o Option) Option {
@@ -12,12 +17,25 @@ func Repo(repo string) func(o Option) Option {
 	}
 }
 
+func FatherHandler(fn func(father of.ID)) func(o Option) Option {
+	return func(o Option) Option {
+		op := o.(option)
+		op.fatherHandler = fn
+		return op
+	}
+}
+
 func Default() Option {
 	return &option{}
 }
 
 type option struct {
-	repo string
+	repo          string
+	fatherHandler func(father of.ID)
+}
+
+func (o option) FatherHandler() func(father of.ID) {
+	return o.fatherHandler
 }
 
 func (o option) Repo() string {
