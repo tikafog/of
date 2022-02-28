792294fd5b8609077f33b0fdcbcba974f3b1aee6
godcong <jumbycc@163.com>
2022-02-28 16:56:21 +0800

    feat(module): add module interface

--- core.go
+++ core.go
@@ -12,5 +12,6 @@ type TypeHandleFunc = func(id string, data json.RawMessage) error
 type Core interface {
 	Connection
 	Context() context.Context
+	Ask(ctx context.Context, data []byte, last int64) error
 	RegisterDataHandler(ct content.Type, fn TypeHandleFunc) error
 }
