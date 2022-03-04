7da1aa47850ef5b3445fc1919005958afe6954e2
godcong <jumbycc@163.com>
2022-03-04 15:15:11 +0800

    feat(module): add module interface

--- event.go
+++ event.go
@@ -3,7 +3,7 @@ package of
 type TypeEventFunc = func(r *EventRequest) error
 
 type EventRequest struct {
-	From Name
-	Type string
-	Data []byte
+	Receiver Name
+	Type     string
+	Data     []byte
 }
