1d17958bfd1297598930054f049de6a27939df21
godcong <jumbycc@163.com>
2022-03-04 17:45:28 +0800

    feat(module): add module interface

--- event.go
+++ event.go
@@ -1,9 +1,11 @@
 package of
 
 type TypeEventFunc = func(r *EventRequest) error
+type TypeEventCallbackFunc = func(v interface{}) error
 
 type EventRequest struct {
 	Receiver Name
 	Type     string
 	Value    interface{}
+	Callback TypeEventCallbackFunc
 }
