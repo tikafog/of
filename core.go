baa6a154edb8a6c40314766b06eecc0492df484e
godcong <jumbycc@163.com>
2022-03-08 13:18:22 +0800

    feat(module): add module interface

--- core.go
+++ core.go
@@ -14,6 +14,7 @@ type Core interface {
 	Event
 	Context() context.Context
 	State() State
+	StoragePath() string
 
 	Inquire(ctx context.Context, r *InquireRequest) error
 	RegisterDataHandler(ct content.Type, fn TypeHandleFunc) error
