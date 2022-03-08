f4fd41e98e769d2b41e61682e20c42a2db0db877
godcong <jumbycc@163.com>
2022-03-08 17:19:41 +0800

    feat(module): add module interface

---
+++ module/notset.go
@@ -0,0 +1,21 @@
+package module
+
+import (
+	"github.com/tikafog/of"
+)
+
+type notsetModule struct{}
+
+func (n notsetModule) Valid() bool {
+	return false
+}
+
+func (n notsetModule) IsRunning() bool {
+	return false
+}
+
+func (n notsetModule) Name() of.Name {
+	return of.NameNotSet
+}
+
+var NotSetModule of.ModuleStater = notsetModule{}
