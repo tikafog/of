a792fb86d70c45d657315e760635fd82ce4b5dae
godcong <jumbycc@163.com>
2022-03-16 18:28:05 +0800

    feat(module): add module interface

--- tool.go
+++ tool.go
@@ -8,3 +8,15 @@ type Identifier interface {
 	Decode(id string) (ID, error)
 	Encode(id ID) string
 }
+
+type Addr interface {
+	ID() ID
+	Addrs() []string
+	StringAddr() []string
+}
+
+type Net interface {
+	ParseStringAddr(addr string) (Addr, error)
+	ParseStringAddrs(addrs ...string) ([]Addr, error)
+	StringAddr(addr Addr) ([]string, error)
+}
