0c7df30c5fa1bf28eb853f32ff5256636449766c
godcong <jumbycc@163.com>
2022-06-13 16:54:59 +0800

    feat(module): add db template

--- db/generate.sh
+++ db/generate.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-DBS=["bootnode"]
+DBS=("bootnode" "kernel")
 DBNAME=$1
 
 SHELL_FOLDER=$(cd "$(dirname "$0")" || exit;pwd)
+echo "Current wd:${SHELL_FOLDER}"
 
-echo "${SHELL_FOLDER}"
+function fileRemove() {
+    rm -vf "$SHELL_FOLDER/$1"/*.go
+    find "$SHELL_FOLDER/$1"/* -type d ! -name 'schema' -print0 | xargs -0 rm -rvf
+    return
+}
 
-if [ -z "$DBNAME" ]; then
-  echo "Usage: ./generate.sh <dbname>"
-  exit 0
-fi
+function schemaGenerate() {
+    go run -mod=mod entgo.io/ent/cmd/ent generate --feature sql/upsert --feature sql/lock \
+--template "$SHELL_FOLDER"/template \
+--template glob="$SHELL_FOLDER/template/*.tmpl" "$SHELL_FOLDER/$1"/schema
+    return
+}
 
+for i in "${!DBS[@]}" ; do
+    dbname=${DBS[$i]}
+    echo "Process $dbname module"
 
-echo "delete old schema file"
-rm -vf "$SHELL_FOLDER"/"$DBNAME"/*.go
-find "$SHELL_FOLDER"/"$DBNAME"/* -type d ! -name 'schema' -print0 | xargs -0 rm -rvf
-echo "generate new schema"
-#ent generate "$SHELL_FOLDER"/"$DBNAME"/ent/schema
-echo -e "ent generate --template $SHELL_FOLDER/internal/template \
---template glob=\"$SHELL_FOLDER/internal/template/*.tmpl\" $SHELL_FOLDER/$DBNAME/schema"
-go run -mod=mod entgo.io/ent/cmd/ent generate --feature sql/upsert --feature sql/lock \
---template "$SHELL_FOLDER"/internal/template \
---template glob="$SHELL_FOLDER/internal/template/*.tmpl" "$SHELL_FOLDER"/"$DBNAME"/schema
+    echo "Delete old schema file"
+    fileRemove "$dbname"
+
+    echo "Generate new schema"
+    schemaGenerate "$dbname"
+done
