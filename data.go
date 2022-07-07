7a1fab0a596e6452dacbea79f1969a2420588caf
godcong <jumbycc@163.com>
2022-07-07 18:18:49 +0800

    fix(dbc): change data handler to generic

--- data.go
+++ data.go
@@ -10,12 +10,12 @@ import (
 // @Description: DataHandler
 type DataHandler[T any, V any] interface {
 	DataUpdateHandler[T, V]
-	DataQueryHandler[T, V]
+	DataQueryHandler
 }
 
-type DataQueryHandler[T any, V any] interface {
-	Query(ctx context.Context, data T, args ...V) ([]byte, error)
-	Last(ctx context.Context, data T, args ...V) int64
+type DataQueryHandler interface {
+	Query(ctx context.Context, limit, offset int64) ([]byte, error)
+	Last(ctx context.Context) int64
 }
 
 type DataUpdateHandler[T any, V any] interface {
