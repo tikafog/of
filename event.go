fdf8c1795ad9a9833bfbb504de2c35954d7df16b
godcong <jumbycc@163.com>
2022-08-08 17:18:44 +0800

    feat(of): update interface

--- event.go
+++ event.go
@@ -6,7 +6,7 @@ type TypeEventCallbackFunc = func(res *EventResult)
 type Event interface {
 	Event(n Name, r *EventRequest) error
 	EventWithCallback(n Name, r *EventRequest) (<-chan EventResult, error)
-	RegisterEventHandler(from Name, fn TypeEventFunc) error
+	RegisterEventHandler(from Name, event string, fn TypeEventFunc) error
 }
 
 type EventRequest struct {
