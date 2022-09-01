fd404974fda8c5f0fee638fd19b35e46bd8a73ae
godcong <jumbycc@163.com>
2022-09-01 16:07:34 +0800

    feat(of): update interface

--- data.go
+++ data.go
@@ -7,13 +7,13 @@ import (
 // DataHandler is a data handler interface\
 type DataHandler[T any, A any] interface {
 	DataUpdateHandler[T, A]
-	DataQueryHandler[A]
+	DataQueryHandler
 }
 
 //DataQueryHandler is a query handler interface
-type DataQueryHandler[A any] interface {
-	Query(ctx context.Context, limit int, last int64, args ...A) ([]byte, error)
-	Last(args ...A) int64
+type DataQueryHandler interface {
+	Query(ctx context.Context, limit int, last int64) ([]byte, error)
+	Last() int64
 }
 
 // DataUpdateHandler is a update handler interface
