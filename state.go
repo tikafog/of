2de2714c9df01f3c0cb1389aa1caa05f945c07fc
godcong <jumbycc@163.com>
2022-09-13 16:24:52 +0800

    feat(of): update interface

--- state.go
+++ state.go
@@ -2,7 +2,7 @@ package of
 
 //State the state of the device
 //ENUM(invalid,init,preload,loading,waiting,running,syncing,syncing_online,syncing_offline,stopping,stopped,error,max)
-type State int
+type State uint32
 
 func (x State) Is(other State) bool {
 	return x == other
