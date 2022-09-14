1690584cd6670aa5f96ac464ccdf76e69a15f167
godcong <jumbycc@163.com>
2022-09-14 23:25:00 +0800

    feat(of): update interface

--- arg.go
+++ arg.go
@@ -1,6 +1,19 @@
 package of
 
+import (
+	"reflect"
+)
+
 type Arg interface {
 	Type() string
 	Value() interface{}
+}
+
+func ParseArgs[T any](args ...Arg) (T, bool) {
+	for i := range args {
+		if args[i].Type() == reflect.TypeOf(*new(T)).Name() {
+			return args[i].Value().(T), true
+		}
+	}
+	return *new(T), false
 }
