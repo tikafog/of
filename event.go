112061762b2fd96ad10e25c39401ebb6b2d59450
godcong <jumbycc@163.com>
2022-09-20 15:53:25 +0800

    feat(of): update interface

--- event.go
+++ event.go
@@ -5,6 +5,7 @@ type TypeEventCallbackFunc = func(res *EventResult)
 
 type Event interface {
 	Trigger(self Name, r *EventRequest) error
+	TriggerAsync(self Name, r *EventRequest) error
 	//Callback(self Name, r *EventRequest) (<-chan EventResult, error)
 	Register(from Name, event string, fn TypeEventFunc) error
 }
