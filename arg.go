15366d983e497e1484f2ef39a4e630822eb9a615
godcong <jumbycc@163.com>
2022-09-21 21:31:04 +0800

    feat(of): update interface

--- arg.go
+++ arg.go
@@ -4,9 +4,33 @@ import (
 	"reflect"
 )
 
+type arg[T any] struct {
+	t reflect.Type
+	v T
+}
+
+//func (a Arg[T]) AssignAble(t reflect.Type) bool {
+//	return a.t.AssignableTo(t)
+//}
+
+func (a arg[T]) Type() reflect.Type {
+	return a.t
+}
+
+func (a arg[T]) Value() any {
+	return a.v
+}
+
 type Arg interface {
 	Type() reflect.Type
 	Value() any
+}
+
+func NewArg[T any](v T) Arg {
+	return &arg[T]{
+		v: v,
+		t: reflect.TypeOf(v),
+	}
 }
 
 func ParseArgs[T any](args ...Arg) (T, bool) {
