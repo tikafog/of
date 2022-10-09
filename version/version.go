cd8dfe49a0efeec647e3ef71cfb215fa83295a94
godcong <jumbycc@163.com>
2022-10-09 13:34:02 +0800

    feat(of): update interface

--- version/version.go
+++ version/version.go
@@ -1,7 +1,10 @@
 package version
 
-//VersionOne json format version number
+// VersionOne json format version number
 const VersionOne = "0.0.1"
 
-//VersionTwo flatbuffers format version number
+// VersionTwo flatbuffers format version number
 const VersionTwo = "0.0.2"
+
+// VersionThree other format version number
+const VersionThree = "0.0.3"
