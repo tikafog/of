611f14abeb77176a026eccc6d7943ab134ae2297
godcong <jumbycc@163.com>
2022-10-20 15:09:00 +0800

    feat(of): update interface

--- event.go
+++ event.go
@@ -3,17 +3,27 @@ package of
 type EventFunc = func(r *EventRequest) error
 type EventCallbackFunc = func(res *EventResult)
 
-type EventTrigger interface {
-	Trigger(key EventKey, opts ...EventRequestOptions) error
+//type EventTrigger interface {
+//	Trigger(key EventKey, opts ...EventRequestOptions) error
+//}
+
+type KeyMaker interface {
+	To(receiver Name, p string) EventKey
 }
 
 type Event interface {
-	EventTrigger
+	KeyMaker(from Name) KeyMaker
 	Register(key EventKey, fn EventFunc) error
+	Trigger(key EventKey, opts ...EventRequestOptions) error
 }
 
 type EventRegister interface {
 	RegisterEvent(Event) error
+}
+
+type EventRequester interface {
+	EventKey
+	EventCallbackFunc
 }
 
 type EventRequest struct {
