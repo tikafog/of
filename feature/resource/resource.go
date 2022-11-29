2f42b32f9168fcf50a7a56d2e5824c4740f85932
godcong <jumbycc@163.com>
2022-11-29 14:49:21 +0800

    feat(of): update interface

--- feature/resource/resource.go
+++ feature/resource/resource.go
@@ -9,8 +9,8 @@ import (
 type Type uint32
 
 type Resource interface {
-	Add(ctx context.Context, path string, ro ...AddOption) (string, error)
+	Add(ctx context.Context, path string, setting AddSetting) (string, error)
 	//Ls(ctx context.Context, path string, ro ...ResourceOption) (string, error)
-	//Remove(ctx context.Context, path string, ro ...ResourceOption) error
+	Remove(ctx context.Context, path string, setting RemoveSetting) error
 	//Validate(ctx context.Context, path string, ro ...ResourceOption) error
 }
--- feature/resource/resource_option.go
+++ feature/resource/resource_option.go
@@ -1,36 +1,11 @@
 package resource
 
-type AddOption func(*AddSetting)
-
 type AddSetting struct {
 	Type         Type
 	ShowProgress bool
 	IgnoreHidden bool
 }
 
-// TypeOption ...
-// @param ResourceType
-// @return ResourceOption
-func TypeOption(ResourceType Type) AddOption {
-	return func(option *AddSetting) {
-		option.Type = ResourceType
-	}
-}
-
-// ShowProgressOption ...
-// @param bool
-// @return ResourceOption
-func ShowProgressOption() AddOption {
-	return func(option *AddSetting) {
-		option.ShowProgress = true
-	}
-}
-
-// IgnoreHiddenOption ...
-// @param bool
-// @return ResourceOption
-func IgnoreHiddenOption() AddOption {
-	return func(option *AddSetting) {
-		option.IgnoreHidden = true
-	}
+type RemoveSetting struct {
+	Type Type
 }
